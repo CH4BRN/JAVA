eecfd7cdc41c14702e4d2146bf937582c90a845a
pierre.antoine <pierre.antoine@CFD-L009>
2019-04-30 14:28:44 +0200

    Reorganized

---
+++ ECLIPSE/ProxyVirtual_Eclipse/src/virtualProxy/IGrosObjet.java
@@ -0,0 +1,8 @@
+package virtualProxy;
+
+public interface IGrosObjet{
+    // à compléter
+	public void premièreMéthodeAyantVraimentBesoinDeGrosObjet();
+	
+	public void secondeMéthodeAyantVraimentBesoinDeGrosObjet();
+}
